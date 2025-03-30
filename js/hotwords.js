33bb7319bef83db8c7480e6038c38a816dd14aee
nsrivast <nsrivast@gmail.com>
2025-03-29 21:00:16 -0400

    First commit

---
+++ js/hotwords.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $(".hot").hover(
+    function () {
+      const keyword = this.getAttribute("data-keyword");
+      const highlightClass = `highlight-${keyword}`;
+      
+      // Find and highlight all elements with the corresponding highlight class
+      $(`.${highlightClass}`).each(function() {
+        // Mark the entire text content as one unit
+        // This preserves spaces between words in the highlight
+        $(".content").mark($(this).text(), {
+          "accuracy": "exactly",
+          "separateWordSearch": false,
+          "className": "marked-text"
+        });
+      });
+    },
+    function () {
+      $(".content").unmark();
+    }
+  );
+
+  $(".section-arrow").on("click", function (event) {
+    $(this).find("img").toggleClass("rotate");
+    $(this).find("img").toggleClass("rotate-reset");
+    $(this).next(".detail").toggleClass("show");
+  });
+});
